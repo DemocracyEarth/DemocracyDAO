b9f52aeb6424552a5220805a0a2fcec68e9076be
samarth30 <samarth.gugnani30@gmail.com>
2020-06-07 20:27:32 +0530

    resolved all the errors

--- buidler.config.js
+++ buidler.config.js
@@ -3,10 +3,8 @@ usePlugin("@nomiclabs/buidler-truffle5");
 require("./scripts/moloch-tasks");
 require("./scripts/pool-tasks");
 
-const INFURA_API_KEY =
-  "https://mainnet.infura.io/v3/497b4b7a120a49e288702128beeb7e8d";
-const MAINNET_PRIVATE_KEY =
-  "7B017931EA2F7DEDD2FADE6BFA2A9AD7DC4EA361C34E286BFF05ADA5C6CA20BB";
+const INFURA_API_KEY = "";
+const MAINNET_PRIVATE_KEY = "";
 const ROPSTEN_PRIVATE_KEY = "";
 
 module.exports = {
